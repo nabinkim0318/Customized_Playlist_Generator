16c0c5a837394fbaeb602662d5e8305784ade9d7
Nabin Kim <32893645+nabinkim0318@users.noreply.github.com>
2023-09-25 01:46:32 -0400

    Update msd_preprocessing.py
    The updated function concatenates all h5 files' numerical values as a dataframe

--- msd_preprocessing.py
+++ msd_preprocessing.py
@@ -15,7 +15,6 @@ def iterate_h5_files(msd_dataset_directory, h5_dataset_output_csv_directory):
                 if df is not None:
                     df_list.append(df)
                     count += 1
-                    
     if df_list:
         concatenated_df = pd.concat(df_list, ignore_index=True)
         concatenated_df.to_csv(h5_dataset_output_csv_directory, index=False)
@@ -23,33 +22,39 @@ def iterate_h5_files(msd_dataset_directory, h5_dataset_output_csv_directory):
     else:
         print("No HDF5 files found or no data to save.")
     print(count)
+    return concatenated_df
 
 
 # still needs to work on constructing and returning a dataframe
 def preprocess_h5_info(file_path):
     try:
         with h5py.File(file_path, 'r') as file:
-            # Define the path to the 'analysis' group
+            # information is under analysis structure 
             analysis_group_path = '/analysis'
 
             # Check if the 'analysis' group exists in the file
             if analysis_group_path in file:
-                # Access the 'analysis' group
                 analysis_group = file[analysis_group_path]
 
-                # Function to print datasets within the 'analysis' group
-                def print_datasets_in_group(group):
-                    print(f'Datasets within {group.name}:')
-                    for dataset_name in group.keys():
-                        dataset = group[dataset_name]
-                        print(f'Dataset Name: {dataset_name}, Shape: {dataset.shape}, Dtype: {dataset.dtype}')
-
-                # Print datasets within the 'analysis' group
-                print_datasets_in_group(analysis_group)
+                # only collecting numeric features at this moment 
+                if 'songs' in analysis_group:
+                    songs_dataset = analysis_group['songs']
+                    songs_data = songs_dataset[0]  
+                    
+                    songs_dict = {}
+                    for field_name in songs_data.dtype.fields:
+                        field_value = songs_data[field_name]
+                        songs_dict[field_name] = field_value
+                    df = pd.DataFrame([songs_dict])
+                    
+                    return df
             else:
                 print(f"'analysis' group not found in {file_path}")
     except Exception as e:
         print(f"Error: {e}")
 
+
+
 msd_dataset_directory = "/Users/soyeonhong/Downloads/MillionSongSubset/"
-iterate_h5_files(msd_dataset_directory)
+h5_dataset_output_csv_directory = "/Users/soyeonhong/Downloads/h5_dataset.csv"
+concatenated_df = iterate_h5_files(msd_dataset_directory, h5_dataset_output_csv_directory)
