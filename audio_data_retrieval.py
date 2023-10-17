5a8dfca2b66b2cead8ff8eeb41e768490df7caf9
Nabin Kim <32893645+nabinkim0318@users.noreply.github.com>
2023-10-16 20:07:15 -0400

    Update audio_data_retrieval.py

--- audio_data_retrieval.py
+++ audio_data_retrieval.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
              "Levitating (feat. DaBaby) Dua Lipa", "I Ain't Worried OneRepublic", "Victoria's Secret Jax", 
              "Clarity Vance Joy", "Waffle House Jonas Brothers", "Big Energy Latto", 
              "If We Ever Broke Up Mae Stephens", "Cold Heart - PNAU Remix Elton John" ]
-    api_key = "AIzaSyC8SXONO7zPJZzyuxRgMJ3ZWXpml1PSQvM"    
+    api_key = ""    
     home_directory = os.path.expanduser("~")
     output_directory = os.path.join(home_directory, "Downloads")
     download_audio_given_list(track_list, output_directory, api_key)
