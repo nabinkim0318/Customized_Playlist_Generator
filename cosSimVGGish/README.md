6633a730335ff2218b1835ec1f2e7dc6e9d5d741
mtsandra <tengofma@gmail.com>
2023-11-17 01:26:42 -0500

    Implementation v2 for VGGish embeddings

--- cosSimVGGish/README.md
+++ cosSimVGGish/README.md
@@ -11,5 +11,20 @@ git commit -m "commit-message"
 git push
 ```
 
+### Update 11/16
+
+Good news: now VGGish embeddings are (num_frames, 128).
+
+Bad news: results are still not ideal. 
+
+Progress:
+- Got embeddings for all frames
+- Feature alignment has been completed
+- Reimplemented similarity function
+
+Embeddings used are postprocessed. Could switch back to pre-whitened embeddings to test. Similarities are obtained through dot product of 2 column vectors (one overall similarity score for each comparison). Might not be accurate due to feature alignment. Maybe for feature alignment, could do clip instead of duplication. 
+
+-------------
 
 Note: currently the embeddings extracted is ONLY the first block (around 1 second), so the result has a lot of room for improvement. 
+
--- cosSimVGGish/similarity.py
+++ cosSimVGGish/similarity.py
@@ -1,35 +1,92 @@
 import tensorflow.compat.v1 as tf
 import numpy as np
+import soundfile as sf
+import vggish_embeddings
+from sklearn.metrics.pairwise import cosine_similarity
 
-def read_embeddings_from_tfrecord(tfrecord_file):
-    # Define the feature name you used when writing the TFRecord.
-    feature_name = 'audio_embedding'
-    record_iterator = tf.python_io.tf_record_iterator(path=tfrecord_file)
-    string_record = next(record_iterator)
-    # Create a list to store the embeddings.
-    embeddings = []
-    example = tf.train.SequenceExample()
-    example.ParseFromString(string_record)
-    hexembed = example.feature_lists.feature_list['audio_embedding'].feature[0].bytes_list.value[0].hex()
-    arrayembed = [int(hexembed[i:i+2],16) for i in range(0,len(hexembed),2)]
-    print(arrayembed)
+# def read_embeddings_from_tfrecord(tfrecord_file):
+#     """read embeddings from tfrecord
 
-    return np.array(arrayembed)
+#     Args:
+#         tfrecord_file: 
 
-def cosine_similarity(embedding1, embedding2):
+#     Returns:
+#         (num_frames, 128) -> 128 is from VGGish
+#     """
+#     # Define the feature name you used when writing the TFRecord.
+#     feature_name = 'audio_embedding'
+#     record_iterator = tf.python_io.tf_record_iterator(path=tfrecord_file)
+#     embedding_matrix = []
+#     print(record_iterator)
+#     for string_record in record_iterator:
+#         #string_record = next(record_iterator)
+#         # Create a list to store the embeddings.
+#         print(f"EMBEDDING: {string_record}")
+#         example = tf.train.SequenceExample()
+#         example.ParseFromString(string_record)
+#         hexembed = example.feature_lists.feature_list['audio_embedding'].feature[0].bytes_list.value[0].hex()
+#         arrayembed = [int(hexembed[i:i+2],16) for i in range(0,len(hexembed),2)]
+#         # print(arrayembed)
+#         embedding_matrix += [arrayembed]
+#     embedding_matrix = np.array(embedding_matrix)
+#     print(f"Embedding matrix: {embedding_matrix} \n Shape: {embedding_matrix.shape}")
+#     return embedding_matrix
+
+
+
+def feature_alignment(embedding1, embedding2):
+    len_embed1 = len(embedding1)
+    len_embed2 = len(embedding2)
+    if len_embed1 > len_embed2:
+        # embedding2 = np.vstack(embedding2, embedding2[:(len_embed1-len_embed2), :])
+        embedding2 = np.tile(embedding2, (len_embed1 // len_embed2, 1))
+        remaining_rows = len_embed1 % len_embed2
+        if remaining_rows > 0:
+            embedding2 = np.concatenate([embedding2, embedding2[:remaining_rows, :]])
+
+    if len_embed1 < len_embed2:
+        # embedding1 = np.vstack(embedding1, embedding1[:(len_embed2-len_embed1), :])
+        embedding1 = np.tile(embedding1, (len_embed2 // len_embed1, 1))
+        remaining_rows = len_embed2 % len_embed1
+        if remaining_rows > 0:
+            embedding1 = np.concatenate([embedding1, embedding1[:remaining_rows, :]])
+    assert len(embedding1) == len(embedding2), f"Your embedding1 shape is {len(embedding1)}. Your embedding2 shape is {len(embedding2)}"
+    return embedding1, embedding2
+        
+
+def cosine_sim(embedding1, embedding2):
     # Calculate cosine similarity between two embeddings.
     # embedding1 = read_embeddings_from_tfrecord(tfrecord1)
     # embedding2 = read_embeddings_from_tfrecord(tfrecord2)
-    
-    dot_product = np.dot(embedding1, embedding2)
-    norm1 = np.linalg.norm(embedding1)
-    norm2 = np.linalg.norm(embedding2)
-    similarity = dot_product / (norm1 * norm2)
-    return similarity
+    embedding1_align, embedding2_align = feature_alignment(embedding1, embedding2)
+    # dot_product = np.dot(embedding1, embedding2.T)
+    # norm1 = np.linalg.norm(embedding1)
+    # norm2 = np.linalg.norm(embedding2)
+    # similarity = dot_product / (norm1 * norm2)
+    # Flatten the matrices
+    flat_matrix1 = embedding1_align.flatten()
+    flat_matrix2 = embedding2_align.flatten()
+
+    # Reshape the flattened arrays to be 2D
+    flat_matrix1 = flat_matrix1.reshape(1, -1)
+    flat_matrix2 = flat_matrix2.reshape(1, -1)
+
+    # Calculate cosine similarity
+    similarity_score = cosine_similarity(flat_matrix1, flat_matrix2)[0, 0]
+
+    return similarity_score
+
+
 
 # if __name__ == '__main__':
-#     # Specify the path to your TFRecord files.
-#     tfrecord_file = ['./vggish-embeddings/01-D_AMairena.tfrecord', './vggish-embeddings/63-M2_AMairena.tfrecord']
+# #     raw_dataset = tf.data.TFRecordDataset("./vggish-embeddings/01-D_AMairena.tfrecord")
+# #     for raw_record in raw_dataset.take(1):
+# #         example = tf.train.Example()
+# #         example.ParseFromString(raw_record.numpy())
+# #         print(f"Hello?{example}")
+#     read_embeddings_from_tfrecord("./vggish-embeddings/ver 1.tfrecord")
+    # Specify the path to your TFRecord files.
+    # tfrecord_file = ['./vggish-embeddings/01-D_AMairena.tfrecord', './vggish-embeddings/63-M2_AMairena.tfrecord']
 
 
 #     # Read the embeddings from the TFRecord files.
