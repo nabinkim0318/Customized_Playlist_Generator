8d8e1d130e3472e9763f831659d73f0b3b84e4d1
mtsandra <tengofma@gmail.com>
2023-12-08 03:45:15 -0500

    no wav files now

--- main.py
+++ main.py
@@ -17,17 +17,7 @@ ALL_METHODS = config.ALL_METHODS
 TOP_N = config.TOP_N
 
 def playlist_generate(high_level = "evaluation", method=ALL_METHODS):
-    
-    if high_level == "similarity":
-        high_level_input.similarity_based_main()
-    if high_level == "random":
-        high_level_input.random_main()
-    if high_level == "evaluation":
-        
-        convert.convert_folder_to_wav("./song_data/")
-        high_level_input.copy_wav_files(source_folder="./song_data/seed_songs/", destination_folder="./song_data/similarity_based")
-        return 
-    # first convert all files into wav
+        # first convert all files into wav
     convert.convert_folder_to_wav(SEED_SONG_FOLDER)
     
     counter = 0
@@ -37,44 +27,23 @@ def playlist_generate(high_level = "evaluation", method=ALL_METHODS):
         subprocess.run(["python3", "similarity_main.py"])
         counter += 1
     vgg = vggish_embeddings.CreateVGGishNetwork(vggish_window)
-    # iterate through each song
-    seed_song_names_col = []
-    folder_names = []
-    methods_col = []
-    output_song_col = []
     if high_level == "similarity":
-        song_subset_folder = os.path.join("..", SEED_SONG_FOLDER, "similarity_based/")
-        # looking into each seed song subfolder
+        high_level_input.similarity_based_main()
+        result_report = get_playlist_report_reorganize_folders(method=method, vgg=vgg, high_level =high_level+"_based")
+    if high_level == "random":
+        high_level_input.random_main()
+        result_report = get_playlist_report_reorganize_folders(method=method, vgg=vgg, high_level=high_level+"_songs")
+    if high_level == "evaluation":
+        
+        convert.convert_folder_to_wav("./song_data/")
+        high_level_input.copy_wav_files(source_folder="./song_data/seed_songs/", destination_folder="./song_data/similarity_based")
+        return 
 
-        for drctry in tqdm([entry.name for entry in os.scandir(song_subset_folder) if entry.is_dir()]):
-            
-            current_seed_song_name = drctry+".wav"
-            seed_song_names_col += [current_seed_song_name] * len(method) * TOP_N
-            current_folder_name_for_method = os.path.join(song_subset_folder, drctry)
-            folder_names += [current_folder_name_for_method]
+    # iterate through each song
 
 
-            print(current_seed_song_name)
-            print(f"METHOD LIST: {method}")
-            sim_dict = similarity_main.similarity(vgg=vgg,seed_song=current_seed_song_name, folder_path = current_folder_name_for_method, method=method)
-            
-            for m, sim_matrix in tqdm(sim_dict.items()):
-                methods_col += [m] * TOP_N
-                output_dir = os.path.join("..",SEED_SONG_FOLDER, "output", m, drctry)
-                if not os.path.exists(output_dir):
-                    os.makedirs(output_dir)
-                output_songs = list(sim_matrix["name"][1:TOP_N+1])
-                output_song_col += output_songs
-                for song in tqdm(output_songs):
-                    if os.path.exists(output_dir):
-                        shutil.copy(os.path.join( current_folder_name_for_method, song), os.path.join( output_dir, song))
-                    else:
-                        print(f"Output directory: {output_dir} does not exist!")
-    print(f"seed song: {len(seed_song_names_col)}, method: {len(methods_col)}, song: {output_song_col}")
-    result_report = pd.DataFrame({"Seed song names": seed_song_names_col, "Similarity method": methods_col, "Recommended song": output_song_col})
-    
-    print(result_report)
-                
+
+    print("hi")           
                 
                 
                 
@@ -86,6 +55,43 @@ def playlist_generate(high_level = "evaluation", method=ALL_METHODS):
 
     return 
 
+def get_playlist_report_reorganize_folders(method, vgg, high_level="similarity_based"):
+    seed_song_names_col = []
+    folder_names = []
+    methods_col = []
+    output_song_col = []
+    song_subset_folder = os.path.join("..", SEED_SONG_FOLDER, high_level)
+    # looking into each seed song subfolder
+
+    for drctry in tqdm([entry.name for entry in os.scandir(song_subset_folder) if entry.is_dir()]):
+        
+        current_seed_song_name = drctry+".wav"
+        seed_song_names_col += [current_seed_song_name] * len(method) * TOP_N
+        current_folder_name_for_method = os.path.join(song_subset_folder, drctry)
+        folder_names += [current_folder_name_for_method]
+
+
+        print(current_seed_song_name)
+        print(f"METHOD LIST: {method}")
+        sim_dict = similarity_main.similarity(vgg=vgg,seed_song=current_seed_song_name, folder_path = current_folder_name_for_method, method=method)
+        
+        for m, sim_matrix in tqdm(sim_dict.items()):
+            methods_col += [m] * TOP_N
+            output_dir = os.path.join("..",SEED_SONG_FOLDER, "output", m, drctry)
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            output_songs = list(sim_matrix["name"][1:TOP_N+1])
+            output_songs = [name.replace(".wav", ".mp3") for name in output_songs]
+            output_song_col += output_songs
+            for song in tqdm(output_songs):
+                if os.path.exists(output_dir):
+                    shutil.copy(os.path.join( current_folder_name_for_method, song), os.path.join( output_dir, song))
+                else:
+                    print(f"Output directory: {output_dir} does not exist!")
+    
+    result_report = pd.DataFrame({"Seed song names": seed_song_names_col, "Similarity method": methods_col, "Recommended song": output_song_col})
+    return result_report
+        
 
 if __name__ == "__main__":
     playlist_generate()
