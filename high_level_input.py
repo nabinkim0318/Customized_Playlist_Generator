162f8b5020b331f7300c6adec12d53c4a798498d
mtsandra <tengofma@gmail.com>
2023-12-08 02:44:05 -0500

    add user_top_items folder

--- high_level_input.py
+++ high_level_input.py
@@ -232,8 +232,8 @@ def get_recommendation_pool(df):
         #print(sub_df_dict, top_100_songs_id)
 
 def similarity_based_main():
-    #get_user_top_items.print_top_items()
-    get_recommendation_pool(add_user_songs_if_not_exists(term=TERM)) # it should creates a seed_song folder and the corresponding top_10 per seed song
+    get_user_top_items.print_top_items()
+    #get_recommendation_pool(add_user_songs_if_not_exists(term=TERM)) # it should creates a seed_song folder and the corresponding top_10 per seed song
 
 
 
@@ -319,4 +319,4 @@ def random_main():
 
 if __name__ == "__main__":
     similarity_based_main()
-    random_main()
+    #random_main()
